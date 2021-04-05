5ba5c0486f3b307ee294e9244a07042c850cef0b
leonid.kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2021-04-05 23:07:23 +0300

    first commit

---
+++ app/src/main/java/lk/game/speedqualitycost/application/MainApplication.kt
@@ -0,0 +1,18 @@
+package lk.game.speedqualitycost.application
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+//    private lateinit var component: WebComponent
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//        component = DaggerWebComponent.create()
+//    }
+//
+//    fun getWebComponent(): WebComponent {
+//        return component
+//    }
+
+}
